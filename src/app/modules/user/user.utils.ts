406e13eafcfb969224e17c734c3b3504fa0f9e8c
rakib <rakib@pop-os.localdomain>
2023-12-16 03:01:18 +0600

    academicFaculty all function created

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,7 +1,7 @@
+// year semesterCode 4digit number
 import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
 import User from './user.model';
 
-//findlast student ID
 const findLastStudentId = async () => {
   const lastStudent = await User.findOne(
     {
@@ -17,15 +17,34 @@ const findLastStudentId = async () => {
     })
     .lean();
 
+  //2030 01 0001
   // eslint-disable-next-line no-undefined
-  return lastStudent?.id.substring(6) ?? undefined; // Assuming id is a property of lastStudent
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
-//generate student ID
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  // first time 0000
+  //0001  => 1
+  let currentId = (0).toString(); // 0000 by deafult
+
+  const lastStudentId = await findLastStudentId();
+  // 2030 01 0001
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6); //01;
+  const lastStudentYear = lastStudentId?.substring(0, 4); // 2030
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6); // 00001
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
   incrementId = `${payload.year}${payload.code}${incrementId}`;
+
   return incrementId;
 };
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import Router from 'express';
 import { StudentRoutes } from '../modules/student/student.route';
 import { UserRoutes } from '../modules/user/user.router';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 
 const router = Router();
 
@@ -41,10 +42,10 @@ const moduleRoutes = [
     path: '/academic-semesters',
     route: AcademicSemesterRoutes,
   },
-  // {
-  //   path: '/academic-faculties',
-  //   route: AcademicFacultyRoutes,
-  // },
+  {
+    path: '/academic-faculties',
+    route: AcademicFacultyRoutes,
+  },
   // {
   //   path: '/academic-departments',
   //   route: AcademicDepartmentRoutes,
